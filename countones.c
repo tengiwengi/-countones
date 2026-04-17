46b71edd05c9d07844f3da9f3cef552181266434
tengiwengi <tengistumur123@gmail.com>
2026-04-16 20:57:13 -0700

    Worked on count ones

--- countones.c
+++ countones.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 
-int main(){
-
+int main() {
+    char UserYN;
     int UserNumber;
     int count = 0;
-    
-    printf("Welcome to the CountOnes program");
-    printf("Please enter a number: \n");
-    scanf("%d", &UserNumber);
 
-    while(UserNumber != 0){
-        int digit = UserNumber % 10;
-        UserNumber /= 10;
-        if (digit == 1){
-            count ++;
+    printf("Welcome to the CountOnes program\n");
+
+    do {
+        count = 0;
+
+        printf("Please enter a number: \n");
+        scanf("%d", &UserNumber);
+
+        while (UserNumber != 0) {
+            int digit = UserNumber % 10;
+            UserNumber /= 10;
+            if (digit == 1) {
+                count++;
+            }
         }
-    }
-    
-    printf("The number of bits set is : %d\n", count);
 
-    char UserYN;
-    printf("Continue (y/n)?:");
-    if (UserYN == 'y')
-    retyurn
-        
+        printf("The number of ones is: %d\n", count);
 
+        printf("Continue (y/n)?: ");
+        scanf(" %c", &UserYN);
+
+    } while (UserYN == 'y');
+
+    printf("Exiting.\n");
     return 0;
-} 
+}
